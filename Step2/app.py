b8e7c8cc2611bc03d0b1d0f6ca5bc1681bd43d25
Alessandro-Tofani-GIT <alessandro.tofani@sourcesense.com>
2026-02-12 14:57:42 +0100

    Update app.py

--- Step2/app.py
+++ Step2/app.py
@@ -1,23 +1,22 @@
-from flask import Flask
-from opentelemetry import metrics
-from opentelemetry.exporter.prometheus import PrometheusMetricsExporter
-from prometheus_client import start_http_server
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
-# Start Prometheus server on a separate port
-start_http_server(8000)  # Port visibile solo all'esterno se App Service lo permette
+# Counter custom
+c = Counter('my_requests_total', 'Total requests')
+
+@app.before_request
+def before_request():
+    c.inc()
 
 @app.route('/')
 def hello():
     return "Hello DevOps Tribe!"
 
-# Metriche custom
-from prometheus_client import Counter
-c = Counter('my_requests_total', 'Total requests')
-@app.before_request
-def before_request():
-    c.inc()
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
