d4a6e9767b496d04d2bbbb4b29df2751f452e773
Alessandro-Tofani-GIT <alessandro.tofani@sourcesense.com>
2026-02-12 15:56:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
 
 
 
